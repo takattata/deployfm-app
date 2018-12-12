ba4810ce6cde5a6d3d584c220833ddb5024848e2
takattata <ticktack016@gmail.com>
2018-12-13 00:05:00 +0900

    [modify] Rename applicationId & package

--- app/src/androidTest/java/com/takasy/deployfm_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/takasy/deployfm_app/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.takasy.deployfm_app
+package com.takasy.deployfm
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
--- app/src/main/java/com/takasy/deployfm/MainActivity.kt
+++ app/src/main/java/com/takasy/deployfm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.takasy.deployfm_app
+package com.takasy.deployfm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
