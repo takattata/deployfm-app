0d2f31aed102c8bd0c6c7b9ff0c99445922755c0
takattata <ticktack016@gmail.com>
2018-12-22 01:24:07 +0900

    Implement libs

---
+++ app/src/test/java/com/takasy/deployfm/parser/DeployFmParserTest.kt
@@ -0,0 +1,15 @@
+package com.takasy.deployfm.parser
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class DeployFmParserTest {
+    @Test
+    fun testParse() {
+        assertThat("".isEmpty()).isTrue()
+    }
+}
