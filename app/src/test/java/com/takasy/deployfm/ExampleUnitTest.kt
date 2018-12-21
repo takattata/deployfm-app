87a8ef38a2c85a132965b28bf6874da3bd80499d
Yuki Anzai <anzai.y.aa@gmail.com>
2018-12-21 18:41:29 +0900

    fix package name

--- app/src/test/java/com/takasy/deployfm/ExampleUnitTest.kt
+++ app/src/test/java/com/takasy/deployfm/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.takasy.deployfm_app
+package com.takasy.deployfm
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
